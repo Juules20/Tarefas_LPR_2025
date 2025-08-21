ca06bb3d448e335be7f8240a35297093acd04f74
Juules20 <jucustodio2005@gmail.com>
2025-08-21 10:14:49 -0300

    retirada de tarefa repetida

--- Aula_08/Exercicio03/obj/Debug/net9.0/Exercicio03.AssemblyInfo.cs
+++ Aula_08/Exercicio03/obj/Debug/net9.0/Exercicio03.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd15456f1e6aaf94a8cb4cdcc3f4549cff85336b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70c2068dc0f7ea3c1fe5a511e3906138b3225fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio03")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
